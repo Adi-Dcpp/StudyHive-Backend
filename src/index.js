2d945070c771a0450458a926f439056eaf5f6f0e
adi <luffy1120adi@gmail.com>
2026-01-13 20:44:36 +0530

    feat: establish mongodb connection with proper startup flow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
+import connectDB from "./db/index.db.js";
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`app is listeing at http://localhost:${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`app is listeing at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Server startup failed: ", err);
+    process.exit(1);
+  });
